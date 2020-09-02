cc575b4ab34d7bdf6401d6998baeb2773ee73e46
Sajeeb <sajeebahamed6@gmail.com>
2020-09-03 02:10:30 +0600

    props, dynamic data pass to component

--- core-react/src/App.js
+++ core-react/src/App.js
@@ -26,20 +26,23 @@ function App() {
           <li> {Person.designation} </li>
         </ul>
       </header>
-      <PersonCom></PersonCom>
+      <PersonCom person="Sajeeb" to="Alex"></PersonCom>
+      <PersonCom person="Santos" to="Pique"></PersonCom>
     </div>
   );
 }
 
-function PersonCom() {
+function PersonCom(props) {
+  const mood = ['angry', 'happy', 'not bad', 'good', 'smily']
+  console.log(props);
   const style = {
     backgroundColor: 'red',
     fontSize: '1rem'
   }
   return(
-    <div>
-        <h1 style={style}>Hey! Man Whats Up?</h1>
-        <h2 style={style}> I'm Good Man!! What About You?</h2>
+    <div style={{ border: '1px solid green', width:'50%', margin:'2rem'}}>
+        <h1 style={style}>Hey! {props.to} Whats Up? Mood: {mood[0]} </h1>
+        <h2 style={style}> I'm Good {props.person} !! What About You? Mood: {mood[4]} </h2>
     </div>
   )
 }
