980d0e3a223eb0098fbf9c57965fc8ff86452ac9
Sajeeb <sajeebahamed6@gmail.com>
2020-09-03 01:23:27 +0600

    intro to component

--- core-react/src/App.js
+++ core-react/src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style={{fontSize:'4rem'}}>
+        <p style={{fontSize:'2rem'}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <ul style={ulStyle}>
@@ -26,8 +26,22 @@ function App() {
           <li> {Person.designation} </li>
         </ul>
       </header>
+      <PersonCom></PersonCom>
     </div>
   );
+}
+
+function PersonCom() {
+  const style = {
+    backgroundColor: 'red',
+    fontSize: '1rem'
+  }
+  return(
+    <div>
+        <h1 style={style}>Hey! Man Whats Up?</h1>
+        <h2 style={style}> I'm Good Man!! What About You?</h2>
+    </div>
+  )
 }
 
 export default App;
