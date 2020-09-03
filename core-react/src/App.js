07aee068d9afa780825d549c32ebc824be205caa
Sajeeb <sajeebahamed6@gmail.com>
2020-09-03 18:54:08 +0600

    dynamic data

--- core-react/src/App.js
+++ core-react/src/App.js
@@ -13,6 +13,11 @@ function App() {
     fontSize: '2rem'
   }
   console.log(Person);
+  const products = [
+    { name: 'Adobe Xd', Price: 'Free' },
+    { name: 'Adobe Photoshop', Price: '$199' },
+    { name: 'Adobe Illustrator', Price: '$299' }
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -28,23 +33,46 @@ function App() {
       </header>
       <PersonCom person="Sajeeb" to="Alex"></PersonCom>
       <PersonCom person="Santos" to="Pique"></PersonCom>
+      <AdobePricing products={products} ></AdobePricing>
+
     </div>
   );
 }
 
 function PersonCom(props) {
   const mood = ['angry', 'happy', 'not bad', 'good', 'smily']
-  console.log(props);
+  console.log('person props',props);
   const style = {
     backgroundColor: 'red',
     fontSize: '1rem'
   }
+  
   return(
-    <div style={{ border: '1px solid green', width:'50%', margin:'2rem'}}>
+    <div>
+      <div style={{ border: '1px solid green', width: '50%', margin: '2rem' }}>
         <h1 style={style}>Hey! {props.to} Whats Up? Mood: {mood[0]} </h1>
         <h2 style={style}> I'm Good {props.person} !! What About You? Mood: {mood[4]} </h2>
+      </div>
     </div>
+    
   )
 }
-
+function AdobePricing(props) {
+  // console.log(props.products);
+  const products = props.products
+  const {name, Price} = props.products
+  const cardStyle = {
+    border: '1px solid green',
+    backgroundColor: 'black',
+    color: '#fff',
+    width: '50%'
+  }
+  return (
+    products.map(pd => <PersonCom key={pd.name} products={pd}></PersonCom>)
+    // <div style={cardStyle}>
+    //     <h1> Name: {name} </h1>
+    //     <p> Price: {Price} </p>
+    // </div>
+  )
+}
 export default App;
