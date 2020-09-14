db0db1caabd1678c3d4431f499f4294b7b4a61c3
Sajeeb <sajeebahamed6@gmail.com>
2020-09-15 00:53:05 +0600

    dep

---
+++ project-gm/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CurrentPosition from './components/CurrentPosition/CurrentPosition';
+
+
+function App() {
+  return (
+    <div>
+        <CurrentPosition></CurrentPosition>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-gm/src/components/CurrentPosition/CurrentPosition.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
+import { useForm } from 'react-hook-form';
+import './CurrentPosition.css'
+require('dotenv').config()
+
+const CurrentPosition = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
+    const mapStyles = {
+        height: "100vh",
+        width: "100%"
+    };
+
+    const [currentPosition, setCurrentPosition] = useState({});
+    // console.log(currentPosition.lat, currentPosition.lng);
+    const success = position => {
+        const currentPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+        setCurrentPosition(currentPosition);
+    };
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(success);
+    }, [])
+
+    const [pop, setPop] = useState({})
+    const onSelect = (pos) => {
+        setPop(pos)
+    }
+
+
+    let api = 'AIzaSyD2YCfY5IwyfITrZo01zPtwDXG__X66A2A'
+    return (
+        <LoadScript
+            googleMapsApiKey={api}
+        >
+            <GoogleMap
+                mapContainerStyle={mapStyles}
+                zoom={13}
+                center={currentPosition}
+            >
+                {
+                    currentPosition &&
+                    <Marker
+                        position={currentPosition}
+                        onClick={() => {
+                            onSelect(currentPosition)
+                        }}
+                    >
+
+                        {
+                            (pop.lat & pop.lng) &&
+                            (
+                                <InfoWindow
+                                    position={pop}
+                                    clickable={true}
+                                    onCloseClick={() => setPop({})}
+                                >
+                                    <div>
+                                        <form className="userInfo" onSubmit={handleSubmit(onSubmit)}>
+                                            <div className="">
+                                                {errors.lat && <li> Lat is required</li>}
+                                                {errors.lng && <li>Lng is required</li>}
+                                                {errors.userName && <li>User Name is required</li>}
+                                                <div className="d-flex">
+                                                    <input defaultValue={pop.lat} name="lat" className="mr-2" ref={register({ required: true })} placeholder="Please Enter Lat" />
+                                                    <input defaultValue={pop.lng} name="lng" ref={register({ required: true })} placeholder="Please Enter Lng" />
+                                                </div>
+                                                <br />
+                                                <input name="userName" ref={register({ required: true })} placeholder="Please Enter Your Name" />
+
+                                                <br />
+                                                <input value="Check Now" type="submit" />
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </InfoWindow>
+                            )
+                        }
+                    </Marker>
+                }
+            </GoogleMap>
+
+        </LoadScript>
+    );
+};
+
+export default CurrentPosition;
