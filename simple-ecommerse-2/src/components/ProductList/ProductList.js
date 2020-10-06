872e45be6bd33d1c1befdb6904d4cfcfd9d630fc
Sajeeb <sajeebahamed6@gmail.com>
2020-10-07 00:42:34 +0600

    refactoring product component

--- simple-ecommerse-2/src/components/ProductList/ProductList.js
+++ simple-ecommerse-2/src/components/ProductList/ProductList.js
@@ -1,31 +1,15 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import ThemeContext from '../Theme/ThemeContext';
+import ListProduct from './ListProduct';
 
-const Product = ({ id, title, brand, price, img_url, addCartItem} ) => {
-    return(
-        <div className="product">
-            <Link to={`/product/${id}`}>
-                <img src={img_url} alt={title} />
-                <div className="title">
-                    <span> {title} </span>
-                    <span> {brand} </span>
-                </div>
-            </Link>
-            <div className="actions">
-                <span> $ {price} </span>
-                <button onClick={() => addCartItem(id)}> Add to cart </button>
-            </div>
-        </div>
-    )
-}
+
 
 const ProductList = ({ products, addCartItem} ) => {
     const {dark} = useContext(ThemeContext)
     return (
         <div className={`product-list ${dark ? 'dark' : 'light'}`}>
             {products.map(product => (
-                <Product {...product} addCartItem={addCartItem}></Product>
+                <ListProduct {...product} addCartItem={addCartItem}></ListProduct>
             ))
             }
         </div>
