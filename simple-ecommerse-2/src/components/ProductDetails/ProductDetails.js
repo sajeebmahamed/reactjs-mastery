90cd993129b2b67f674ed8d24537df104bdbb021
Sajeeb <sajeebahamed6@gmail.com>
2020-10-07 01:21:28 +0600

    product details added

--- simple-ecommerse-2/src/components/ProductDetails/ProductDetails.js
+++ simple-ecommerse-2/src/components/ProductDetails/ProductDetails.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import data from '../../data';
+import ListProduct from '../ProductList/ListProduct';
 
 const ProductDetails = () => {
+    const { productId } = useParams()
+    const product = data.find(p => p.id === parseInt(productId))
+    console.log(product);
     return (
-        <div>
-            <h3> Product Details </h3>
+        <div className="product-details">
+            <ListProduct {...product} />
         </div>
     );
 };
