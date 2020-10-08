91d9386ef93af8e15f57dc26609fd30e6ffc6ead
Sajeeb <sajeebahamed6@gmail.com>
2020-10-08 21:48:15 +0600

    :p

---
+++ emailjs-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
