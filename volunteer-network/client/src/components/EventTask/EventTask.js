c1182ce09a728f34f2baf87fcf963064010fbc51
Sajeeb <sajeebahamed6@gmail.com>
2020-10-26 02:46:29 +0600

    skeleton added on events component

--- volunteer-network/client/src/components/EventTask/EventTask.js
+++ volunteer-network/client/src/components/EventTask/EventTask.js
@@ -1,7 +1,8 @@
-import { Button, Container, CssBaseline, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Box, Button, CircularProgress, Container, CssBaseline, LinearProgress, makeStyles, Paper, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import eventImg from '../../images/childSupport.png'
+import Skeleton from '@material-ui/lab/Skeleton';
 const axios = require('axios');
 
 const useStyles = makeStyles((theme) => ({
@@ -32,7 +33,7 @@ const EventTask = (props) => {
 
     const handleCancel = (id) => {
         axios.delete('http://localhost:4000/cancel/' + id)
-            .then(res=> {
+            .then(res => {
                 console.log(res);
             })
     }
@@ -43,19 +44,41 @@ const EventTask = (props) => {
             <CssBaseline />
             <Container maxWidth="md">
                 <Grid container spacing={3}>
-                    { 
-                         registeredEvent.map(event => (
+                    {
+                        registeredEvent.map(event => (
                             <Grid item xs={6}>
                                 <Paper className={classes.paper}>
                                     <div className={classes.event}>
-                                        <img width="20%" src={eventImg} alt="" />
+                                        {
+                                            event === 0 ? <Skeleton variant="rect" width={210} height={118} />
+                                            :
+                                            <img width="20%" src={eventImg} alt="" />
+                                        }
+                                        
                                         <div>
-                                            <Typography variant="h6" align="left" style={{ marginLeft: "1rem" }}> {event.event} </Typography>
-                                            <Typography variant="body1" align="left" style={{ marginLeft: "1rem" }}> {event.date} </Typography>
-                                            <Button
-                                                 onClick={() => handleCancel(event._id)}
-                                                variant="contained"
-                                                color="info" size="small" style={{ marginLeft: "15rem" }}> Cancel </Button>
+                                            {
+                                                event === 0 ?
+                                                    <Box ml={1}>
+                                                        <Skeleton animation="wave" width="40%" />
+                                                        <Skeleton width="40%" />
+                                                    </Box>
+                                                :
+                                                <div>
+                                                        <Typography variant="h6" align="left" style={{ marginLeft: "1rem" }}> {event.event} </Typography>
+                                                        <Typography variant="body1" align="left" style={{ marginLeft: "1rem" }}> {event.date} </Typography>
+                                                </div>
+                                            }
+                                            {
+                                                event === 0 ?
+                                                    <Skeleton variant="rect" width="40%" height="20%" style={{ marginLeft: "8rem", marginTop: '3rem' }} />
+                                                    :
+                                                    <Button
+                                                        onClick={() => handleCancel(event._id)}
+                                                        variant="contained"
+                                                        color="info" size="small" style={{ marginLeft: "15rem" }}>
+                                                        Cancel
+                                                    </Button>
+                                            }
                                         </div>
                                     </div>
                                 </Paper>
