a0d9a8c180d55275a3e305e6ed4e3c9ecd1bc5e2
Sajeeb <sajeebahamed6@gmail.com>
2020-10-28 00:09:48 +0600

    store creation done

---
+++ simple-ecommerse-2/src/store.js
@@ -0,0 +1,31 @@
+import React, {createContext} from 'react'
+import { useReducer } from 'react'
+
+const init = {
+    cartItems: []
+}
+
+const store = createContext(init)
+const { Provider } = store
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'SET_CART_ITEMS':
+            return {
+                ...state,
+                cartItems: action.payload
+            }
+            default:
+                return state
+    }
+}
+
+const StateProvider = ({children}) => {
+    const {state, dispatch} = useReducer(reducer, init)
+    return <Provider value= {{ state, dispatch }}> {children} </Provider>
+}
+
+export {
+    store,
+    Provider
+}
