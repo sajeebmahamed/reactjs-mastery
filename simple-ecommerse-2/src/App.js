7b7531c2da6155552d8b922548b48d6f944a8029
Sajeeb <sajeebahamed6@gmail.com>
2020-11-03 02:13:59 +0600

    code splitting || lazy

--- simple-ecommerse-2/src/App.js
+++ simple-ecommerse-2/src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ThemeContext from './components/Theme/ThemeContext'
-import ProductDetails from './components/ProductDetails/ProductDetails'
+// import ProductDetails from './components/ProductDetails/ProductDetails'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CheckOut from './components/CheckOut/CheckOut';
-import Home from './components/Home/Home';
+// import CheckOut from './components/CheckOut/CheckOut';
+// import Home from './components/Home/Home';
 import { StateProvider } from './store';
-import Cart from './components/Cart/Cart';
+// import Cart from './components/Cart/Cart';
+
+const Home = lazy(() => import('./components/Home/Home'))
+const CheckOut = lazy(() => import('./components/CheckOut/CheckOut'))
+const ProductDetails = lazy(() => import('./components/ProductDetails/ProductDetails'))
+const Cart = lazy(() => import('./components/Cart/Cart'))
 
 function App() {
   const [dark, setDark] = useState(false);
@@ -20,13 +25,15 @@ function App() {
       <ThemeContext.Provider value={{ dark: dark, toggle: toggleDark }}>
         <div>
           <Router>
-            <Navbar></Navbar>
-            <Switch>
-              <Route path="/checkout" component={CheckOut} />
-              <Route path="/product/:productId" component={ProductDetails} />
-              <Route path="/" component={Home} />
-            </Switch>
-            <Cart />
+            <Navbar />
+            <Suspense fallback={<div>loading</div>}>
+              <Switch>
+                <Route path="/checkout" component={CheckOut} />
+                <Route path="/product/:productId" component={ProductDetails} />
+                <Route path="/" component={Home} />
+              </Switch>
+              <Cart />
+            </Suspense>
           </Router>
         </div>
       </ThemeContext.Provider>
