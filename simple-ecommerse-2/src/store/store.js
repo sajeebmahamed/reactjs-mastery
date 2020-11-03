10de6551ce4d207e00f2dec40902aa4842cf5e13
Sajeeb <sajeebahamed6@gmail.com>
2020-11-04 00:03:18 +0600

    redux persist added

--- simple-ecommerse-2/src/store/store.js
+++ simple-ecommerse-2/src/store/store.js
@@ -2,15 +2,29 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' //
+import { PersistGate } from 'redux-persist/integration/react'
 
-
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cartItems']
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
 // const store = createStore(reducer)
 const store = createStore(
-    reducer, /* preloadedState, */
+    persistedReducer, /* preloadedState, */
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+const persistor = persistStore(store)
+
 const StateProvider = ({ children }) => {
-    return <Provider store={store}> {children} </Provider>
+    return (<Provider store={store}>
+        <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+            {children}
+        </PersistGate>
+    </Provider>)
 }
 
 export {
